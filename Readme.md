dd1c249891951a132436e567951cfc17c72fcba1
Fabian Bentz <info@fabianbentz.de>
2022-11-04 13:46:21 +0100

    add further functionality

--- Readme.md
+++ Readme.md
@@ -1,5 +1,27 @@
-# Convert .csv to .md files
+# Nodejs CSV to Markdown converter
 
-This little script converts each row of a .csv to a single .md markdown file to use at static site generators.
+This little helper script converts every row of a give CSV file to simple Markdown files.
 
-Documentation and more coming soon…
+This will NOT create a Markdown table.
+
+## Usage
+Just clone this repository and run ``npm install``, after the installation of all dependencies run ``node index.js``.
+
+### Template
+You can just update the ``basic.md`` template to fit your needs or just add another template file.
+All templates must be placed at the template directory.
+
+### Schema
+To map your CSV columns to the template we use a simple ``schema`` object. 
+
+### User input
+
+You get promoted to enter some data:
+
+1. ``path`` to your .csv file, e.g. ``./demo.csv``
+2. ``templateName`` the name of your template, placed at the template directory, default value ``basic.md``
+3. ``from`` to line where to start from at your .csv file, default value ``1``
+4. ``till`` to line where to stop at your .csv file, default value ``null`` to stop at the end of the .csv
+5. ``userColumns`` possible answers ``y``or ``N``, default value ``y``
+
+then your csv. file will be parsed and all markdown files will be generated at the md-files directory.
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
-const slugify = require('slugify')
-let { render } = require("mustache")
+const slugify = require('slugify');
+const prompt = require('prompt-sync')({sigint: true});
+const clc = require("cli-color");
+let { render } = require("mustache");
 
 
 // Parse the .csv file and return the data
@@ -24,32 +26,95 @@ const parseCsvFile = async (file, from, to = null, useColumns = false) => {
     return records
 }
 
-// Parse .csv to get column and row data and start .md file generation
-parseCsvFile('./demo.csv', 2, null, true).then(
-    (res) => {
-        generateMd(res);
-    }
-).catch(
-    (err) => {
-        console.log('rows:', err);
-    }
-);
-
 // Generate .md files based on given data
-const generateMd = (rows) => {
-    let template = fs.readFileSync("./template/basic.md").toString()
+const generateMd = (rows, templateName) => {
+
+    if (!rows.length) {
+        console.log('No variables could be generated from the CVS columns, please check your CSV and try again.');
+        return;
+    }
+
+    let schema = {};
+    const keys = Object.keys(rows[0]);
+
+    if (!keys.length) {
+        console.log('No variables could be generated from the CVS columns, please check your CSV and try again.');
+        return;
+    }
+
+    const mdVariables = prompt(`Available markdown variables: ${clc.red(keys.join(','))}, please update your markdown template. Updated? (y/N):` , 'y', {autocomplete: complete(['y', 'N'])});
+    if (mdVariables !== 'y') {
+        console.log('Markdown template not updated, please update and restart.');
+        return;
+    }
+
+    let selectedKey = prompt('Which variable should be used to generate slug and file name? ', 'id', {autocomplete: complete(keys)});
+
+    if (!keys.includes(selectedKey)) {
+        console.log(`Key are not part of your available variables: ${clc.red(keys.join(', '))}, fallback is used.`, clc.red(selectedKey));
+        if (!selectedKey) { selectedKey = schema[0]; }
+    } else {
+        console.log('Slug and filename generated based on key: ', clc.red(selectedKey));
+    }
+
+    let template = fs.readFileSync(`./template/${templateName}`).toString();
     rows.forEach( (row) => {
-        const page = {
-            id: row.id,
-            title: row.title,
-            slug: slugify(row.title, {lower: true, strict: true}),
-            genres: row.genres,
-            release: row.release,
-            description: row.description,
-        }
-        console.log(page);
-        let output = render(template, page)
-        fs.writeFileSync(`./md-files/${page.slug}.md`, output)
+
+        keys.forEach((key, i) => {
+            schema[key] = row[key] ? row[key] : '';
+        });
+
+        schema.slug = slugify(row[selectedKey], {lower: true, strict: true});
+
+        let output = render(template, schema)
+        fs.writeFileSync(`./md-files/${schema.slug}.md`, output);
+        console.log(`~ Markdown file ${schema.slug}.md generated.`);
     })
+
 }
 
+// Init parser and get user input
+const initCsvParser = () => {
+
+    const path = prompt('Enter path to .csv file (e.g. ./demo.csv): ');
+    console.log('Your path: ', clc.red(path));
+
+    const templateName = prompt('Enter template name: ', 'basic.md');
+    console.log('Your template name: ', clc.red(templateName));
+
+    const from = prompt('Enter start from line: ', 1);
+    console.log('Your start from Line: ', clc.red(Number(from)));
+
+    const till = prompt('Enter end line: ', null);
+    console.log('Your end Line: ', till ? clc.red(till) : clc.red('runs till the end of the file'));
+
+    console.log('The CSV column names are used for variable and schema generation!');
+    const useColumns = 'y';
+
+    // Parse .csv to get column and row data and start .md file generation
+    parseCsvFile(path, from, till ? Number(till) : null, useColumns).then(
+        (res) => {
+            generateMd(res, templateName);
+        }
+    ).catch(
+        (err) => {
+            console.log('rows:', err);
+        }
+    );
+
+}
+
+initCsvParser();
+
+// Helper functions
+function complete(commands) {
+    return function (str) {
+        let i;
+        let ret = [];
+        for (i=0; i< commands.length; i++) {
+            if (commands[i].indexOf(str) === 0)
+                ret.push(commands[i]);
+        }
+        return ret;
+    };
+}
